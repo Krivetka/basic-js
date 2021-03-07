9d230e4b8b8d63ab5ce1428b30f88af341228698
IVAN KULIASHOV <42475953+Krivetka@users.noreply.github.com>
2021-03-07 18:22:42 +0300

    feat: add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(mass) {
+  if(mass!=null){
+    text_arr=[];
+    str='';
+         for(i=0; i<mass.length; i++){
+         if(typeof mass[i]==='string'){
+           mass[i]=mass[i].trim();
+             char = mass[i].charAt(0).toUpperCase();
+                 text_arr.push(char);
+         }
+     };
+    text_arr.sort();
+    for(j=0; j<text_arr.length; j++){
+        str+=text_arr[j];
+    }
+    if(str!= '') return str;
+  }
+  return false;
 };
